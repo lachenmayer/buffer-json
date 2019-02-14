b0559d5215af4d84bdddd88e1e848b7a2ee1fbd4
harry lachenmayer <harrylachenmayer@gmail.com>
2019-02-14 14:38:22 +0000

    rewrite

--- test.js
+++ test.js
@@ -1,46 +1,45 @@
-var assert = require('assert')
-var semver = require('semver')
-var bufferJson = require('./')
-var fixtures = require('./fixtures')
+const test = require('tape')
+const BJSON = require('.')
 
-/* global before, describe, it */
+test("don't touch anything other than buffers", t => {
+  t.deepEquals(
+    BJSON.stringify({ foo: 'bar', baz: 5, removeMe: undefined, boing: null }),
+    JSON.stringify({ foo: 'bar', baz: 5, removeMe: undefined, boing: null })
+  )
+  t.end()
+})
 
-describe('buffer-json', function () {
-  before(function () {
-    assert(semver.gte(process.version, '0.11.0'), 'Bad Node.js/IO.js version. You need to use at least v0.11.')
-  })
+test('buffers come out unscathed', t => {
+  const bufs = [
+    Buffer.from('foo'),
+    Buffer.from(''),
+    Buffer.from('🌈'),
+    { buf: Buffer.from('🌈'), test: 'yep' }
+  ]
+  for (const buf of bufs) {
+    t.deepEquals(BJSON.parse(BJSON.stringify(buf)), buf)
+  }
+  for (const buf of bufs) {
+    t.deepEquals(BJSON.parse(JSON.stringify(buf)), buf)
+  }
+  t.end()
+})
 
-  describe('reviver()', function () {
-    fixtures.valid.forEach(function (f) {
-      it('should convert ' + f.utf8, function () {
-        // verify fixture is correct
-        var b = new Buffer(f.utf8)
-        assert.deepEqual(b.toJSON(), f.json)
+test('utf8', t => {
+  const str = '{"foo":{"type":"Buffer","data":"🌈"}}'
+  t.deepEquals(BJSON.parse(str), { foo: Buffer.from('🌈') })
+  t.end()
+})
 
-        var b2 = JSON.parse(JSON.stringify(f.json), bufferJson.reviver)
-        assert.strictEqual(b.toString('utf8'), b2.toString('utf8'))
-        assert.strictEqual(b.toString('hex'), b2.toString('hex'))
-        assert.strictEqual(b.toString('base64'), b2.toString('base64'))
-
-        var b3 = JSON.parse(JSON.stringify(f.json2), bufferJson.reviver)
-        assert.strictEqual(b3.toString('utf8'), b2.toString('utf8'))
-        assert.strictEqual(b3.toString('hex'), b2.toString('hex'))
-        assert.strictEqual(b3.toString('base64'), b2.toString('base64'))
-      })
-    })
-  })
-
-  describe('replacer', function () {
-    fixtures.valid.forEach(function (f) {
-      it('should convert ' + f.utf8, function () {
-        var b = new Buffer(f.utf8)
-        assert.deepEqual(b.toJSON(), f.json)
-        assert.notDeepEqual(b.toJSON(), f.json2)
-
-        // with new replacer
-        assert.notDeepEqual(JSON.stringify(b.toJSON()), JSON.stringify(f.json2))
-        assert.deepEqual(JSON.stringify(b, bufferJson.replacer), JSON.stringify(f.json2))
-      })
-    })
-  })
+test('not actually a buffer', t => {
+  const almosts = [
+    { type: 'Buffer' },
+    { type: 'Buffer', data: 500 },
+    { type: 'Buffer', whatever: [123, 124, 125] }
+  ]
+  for (const almost of almosts) {
+    const str = JSON.stringify(almost)
+    t.deepEquals(BJSON.parse(str), almost)
+  }
+  t.end()
 })
